81487ff11520442199308dfe21aae9e03708c1fc
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2025-12-29 00:20:21 +0530

    Signup frontend functional

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
+import Signup from '../Profile/Signup'
 
 const Home = () => {
   return (
     <div>
         
       Home Page of soso
+      <Signup/>
 
     </div>
   )
--- frontend/src/components/Profile/Signup.jsx
+++ frontend/src/components/Profile/Signup.jsx
@@ -1,16 +1,51 @@
 import React from 'react'
-
+import axios from 'axios'
+import { useState } from 'react'
+const url=import.meta.env.VITE_baseurl+'/users/register'
 const Signup = () => {
+  const [firstName,setfn]=useState("")
+  const [lastName,setln]=useState("")
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+  const [cp,setCp]=useState("")
+  const [phone,setPhone]=useState("")
+  const [bio,setBio]=useState("")
+
+
+
+  const register=async(e)=>{
+    e.preventDefault()
+    if (cp.trim() === password.trim()){
+      const resp=await axios.post(url,{firstName,lastName,email,password,phone,bio})
+    console.log(resp)
+
+    }
+    else{
+      alert("Passwords do not match");
+    return;
+    }
+    
+
+
+  }
   return (
     <div>
-        <form className='signup'>
-            <label>First Name <input type="text" /></label>
-            <label>Last Name <input type="text" /></label>
-            <label>Email <input type="email" /></label>
-            <label>Password <input type="password" /></label>
-            <label>Confirm Password <input type="pasword" /></label>
-            <label>Phone <input type="text" /></label>
-            <label>Bio <input type="text" /></label>
+        <form className='signup' onSubmit={register}>
+            <label>First Name <input type="text" value={firstName} onChange={(e)=>setfn(e.target.value)}/></label>
+            <br />
+            <label>Last Name <input type="text" value={lastName} onChange={(e)=>setln(e.target.value)}/></label>
+            <br />
+            <label>Email <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)}/></label>
+            <br />
+            <label>Password <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} /></label>
+            <br />
+            <label>Confirm Password <input type="password" value={cp} onChange={(e)=>setCp(e.target.value)}/></label>
+            <br />
+            <label>Phone <input type="text" value={phone} onChange={(e)=>setPhone(e.target.value)}/></label>
+            <br />
+            <label>Bio <input type="text" value={bio} onChange={(e)=>setBio(e.target.value)} /></label>
+            <br />
+            <button type='submit'>Submit</button>
         </form>
       
     </div>
